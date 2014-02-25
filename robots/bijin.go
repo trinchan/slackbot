ae5a214ab120ba1d8816a6b337ff94ce43fb8762
Andrew Schwartz <andrew.schwartz@gengo.com>
2014-02-25 08:19:47 +0000

    Error handling

--- robots/bijin.go
+++ robots/bijin.go
@@ -19,7 +19,6 @@ type BijinConfiguration struct {
 
 var BijinConfig = new(BijinConfiguration)
 
-// Loads the config file and registers the bot with the server for command /bijint.
 func init() {
     flag.Parse()
     configFile := filepath.Join(*ConfigDirectory, "bijin.json")
@@ -40,44 +39,41 @@ func init() {
     RegisterRobot("/bijin", func() (robot Robot) { return new(BijinBot) })
 }
 
-// All Robots must implement a Run command to be executed when the registered command is received.
 func (r BijinBot) Run(command *SlashCommand) (slashCommandImmediateReturn string) {
-    // If you (optionally) want to do some asynchronous work (like sending API calls to slack)
-    // you can put it in a go routine like this
     go r.DeferredAction(command)
-    // The string returned here will be shown only to the user who executed the command
-    // and will show up as a message from slackbot.
     return ""
 }
 
 func (r BijinBot) DeferredAction(command *SlashCommand) {
-    // Let's use the IncomingWebhook struct defined in definitions.go to form and send an
-    // IncomingWebhook message to slack that can be seen by everyone in the room. You can
-    // read the Slack API Docs (https://api.slack.com/) to know which fields are required, etc.
-    // You can also see what data is available from the command structure in definitions.go
     response := new(IncomingWebhook)
     response.Channel = command.Channel_ID
     response.Username = "Bijin Bot"
+    response.Icon_Emoji = ":ghost:"
+    response.Unfurl_Links = true
+
     rand.Seed(time.Now().UTC().UnixNano())
-    loc, err := time.LoadLocation(BijinConfig.Timezone)
-    t := time.Now().In(loc)
+    var t time.Time
+    if BijinConfig.Timezone != "" {
+        loc, err := time.LoadLocation(BijinConfig.Timezone)
+        if err == nil {
+            t = time.Now().In(loc)
+        } else {
+            t = time.Now()
+            response.Text = fmt.Sprintf("ERROR: Unknown timezone (%s) - Serving UTC", BijinConfig.Timezone)
+            MakeIncomingWebhookCall(response)
+        }
+    } else {
+        t = time.Now()
+        response.Text = fmt.Sprintf("WARNING: No timezone set - Serving UTC")
+        MakeIncomingWebhookCall(response)
+    }
     hours := fmt.Sprintf("%02d", t.Hour())
     minutes := fmt.Sprintf("%02d", t.Minute())
     links := []string{"http://www.bijint.com/jp/tokei_images/", "http://www.bijint.com/jp/img/clk/"}
-    if err == nil {
-        response.Text = fmt.Sprintf("<@%s|%s> Here's your <%s%s%s.jpg|%s:%s 美人>", command.User_ID, command.User_Name, links[rand.Intn(len(links))], hours, minutes, hours, minutes)
-    } else {
-        response.Text = fmt.Sprintf("ERROR")
-    }
-
-    response.Icon_Emoji = ":ghost:"
-    response.Unfurl_Links = true
-    // response.Parse = "full"
+    response.Text = fmt.Sprintf("<@%s|%s> Here's your <%s%s%s.jpg|%s:%s 美人>", command.User_ID, command.User_Name, links[rand.Intn(len(links))], hours, minutes, hours, minutes)
     MakeIncomingWebhookCall(response)
 }
+
 func (r BijinBot) Description() (description string) {
-    // In addition to a Run method, each Robot must implement a Description method which
-    // is just a simple string describing what the Robot does. This is used in the included
-    // /c command which gives users a list of commands and descriptions
     return "Displays the current time's 美人 (hope bijint.com doesn't get mad me)\n\tUsage: /bijin\n\tExpected Response: (beautiful woman)"
 }
